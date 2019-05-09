d125d8a125a3c9f6203e9c01e49929f12d427f60
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-05-09 12:58:06 +0100

    Update timer.js

--- Tesra/commands/timer.js
+++ Tesra/commands/timer.js
@@ -11,10 +11,10 @@ if(args[0] <= 0){
 return message.channel.send(`**Please enter a period of time, with either \`s,m,h,d or w\` at the end!**`);
 }
 
-message.channel.send(`**Timer has been set for**: ` + `${ms(ms(NTimer), {long: true})}`)
+message.channel.send(`**Timer has been set for**: ` + `${ms(ms(NTimer), {long: true})}`);
 
 setTimeout(function(){
-message.channel.send(`**Timer has ended, it was set for**: ${ms(ms(NTimer), {long: true})}` + message.author.toString())
+message.channel.send(`**Timer has ended, it was set for**: ${ms(ms(NTimer), {long: true})}` + message.author.toString());
 
 }, ms(NTimer));
 
