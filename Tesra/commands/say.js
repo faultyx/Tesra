c367a40f43bdd68b092a3a08ded454da9f98be6e
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-05-09 12:56:16 +0100

    Update say.js

--- Tesra/commands/say.js
+++ Tesra/commands/say.js
@@ -1,5 +1,5 @@
 exports.run = async (bot, message, args) => {
   let sayMsg = args.join(" ");
   if(!sayMsg) return message.reply("Supply some text!");
-  message.channel.send(sayMsg)
+  message.channel.send(sayMsg);
 };
