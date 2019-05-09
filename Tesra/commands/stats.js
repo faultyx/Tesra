4c7afd644efce08b767013e44f59551aa70fecee
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-05-09 12:57:16 +0100

    Update stats.js

--- Tesra/commands/stats.js
+++ Tesra/commands/stats.js
@@ -5,7 +5,7 @@ const { version } = module.require("discord.js");
 
 exports.run = async (bot, message, args) => {
 
-const arch = os.arch()
+const arch = os.arch();
 const used = process.memoryUsage().heapUsed / 1024 / 1024;
 
 let totalSeconds = process.uptime();
@@ -14,6 +14,6 @@ let days = Math.floor((totalSeconds % 31536000) / 86400);
 let hours = Math.floor((totalSeconds / 3600) % 24);
 let mins = Math.floor((totalSeconds / 60) % 60);
 let ping = bot.ping;
-message.channel.send(`**My Stats**:\nMem Usage: **${Math.round(used * 100) / 100} MB**\nUptime: **Days: ${days} | Hours: ${hours} | Minutes: ${mins} | Seconds: ${seconds}**\nNode: **${process.version}**\n Library: **v${version}**\nArch: **${arch}**\nPlatform: **${os.platform}**\nServers: **${bot.guilds.size}**\nUsers: **${bot.users.size}**\nChannels: **${bot.channels.size}**\nPing: **${ping}ms**\n`)
+message.channel.send(`**My Stats**:\nMem Usage: **${Math.round(used * 100) / 100} MB**\nUptime: **Days: ${days} | Hours: ${hours} | Minutes: ${mins} | Seconds: ${seconds}**\nNode: **${process.version}**\n Library: **v${version}**\nArch: **${arch}**\nPlatform: **${os.platform}**\nServers: **${bot.guilds.size}**\nUsers: **${bot.users.size}**\nChannels: **${bot.channels.size}**\nPing: **${ping}ms**\n`);
 
-}
+};
