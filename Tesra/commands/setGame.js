2d4eecb9d973f4af629992efbfda39ab3a307ad7
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-05-09 12:56:38 +0100

    Update setGame.js

--- Tesra/commands/setGame.js
+++ Tesra/commands/setGame.js
@@ -1,11 +1,11 @@
 const ID = module.require("../settings/credentials.json");
 exports.run = async (bot, message, args) => {
   if (message.author.id !== ID.ownerID) {
-    return message.reply("OWNER ONLY")
+    return message.reply("OWNER ONLY");
   } else {
   let gameName = args.join(" ");
   if(!gameName) return message.reply("**Supply some text**");
-  bot.user.setActivity(gameName)
-  message.channel.send(`**${gameName}** is now my playing game.`)
+  bot.user.setActivity(gameName);
+  message.channel.send(`**${gameName}** is now my playing game.`);
 }
 };
