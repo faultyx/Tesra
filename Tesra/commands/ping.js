dba0d66b0714967d22fd180392241f2b9327b65b
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-10-05 12:55:58 +0100

    changes

--- Tesra/commands/ping.js
+++ Tesra/commands/ping.js
@@ -1,4 +1,4 @@
-exports.run = async (bot, message, args) => {
-  let msg = await message.channel.send("Pinging")
-  msg.edit(`Pong! **${msg.createdTimestamp - message.createdTimestamp}ms**`);
+module.exports.run = async (bot, message, args) => {
+  let msg = await message.channel.send("Pinging");
+  msg.edit(`Pong! **${bot.ping}ms**`);
 };
